4c80de57fd25e1b1361965820eb97acc9d43e7d3
Farid Ghanbari <farid.ghsm@gmail.com>
2014-05-14 22:28:33 +0200

    Program.cs

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Salam Doktor Sexisaeedi");
+            System.Console.WriteLine("Ye dokme bezano boro bede");
+            System.Console.ReadKey();
+
+        }
+    }
+}
