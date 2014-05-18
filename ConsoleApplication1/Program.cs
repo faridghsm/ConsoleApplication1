448a7b1d68fc8720b105e050474df5a00456d0d9
Farid Ghanbari <farid.ghsm@gmail.com>
2014-05-18 14:10:41 +0200

    modified:   Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,10 +11,42 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Salam Doktor Sexisaeedi");
-            System.Console.WriteLine("Ye dokme bezano boro bede");
+            int number= 32; // the chosen number
+            int d=16; // the differing value
+
+            System.Console.WriteLine("Choose a number between 1 and 64, when done, press any key!");
             System.Console.ReadKey();
 
+            for (int i = 0; i < 5; i++ )
+            {
+                System.Console.WriteLine("Is your number > {0}? (y/n)", number);
+                char c = Console.ReadKey().KeyChar; //reads the answer from the user
+                
+                if (c.Equals('y'))
+                    {
+                    number = number + d;
+                    d = d / 2;
+                    }
+                else
+                    {
+                    number = number - d;
+                    d = d / 2;
+                    }
+                System.Console.WriteLine("\n");
+            }
+            
+            System.Console.WriteLine("Is your number {0}?(y/n)", number);
+            char ch = Console.ReadKey().KeyChar;
+            System.Console.WriteLine("\n");
+
+                if (ch.Equals('y'))
+                {
+                System.Console.WriteLine("Your number is {0}", number);
+                }
+            else
+                System.Console.WriteLine("Your number is {0}", number + 1);
+
+                System.Console.ReadKey();
         }
     }
 }
