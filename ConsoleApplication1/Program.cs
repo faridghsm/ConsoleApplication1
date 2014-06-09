00c967c869152c2be8a4ae4a14bd332921be2719
Farid Ghanbari <farid.ghsm@gmail.com>
2014-06-09 15:42:10 +0200

    modified:   Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,56 +12,40 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            System.Console.WriteLine("Choose a natural number, when done, type a maximum value!");
+            System.Console.WriteLine("Choose a (not negative) number, when done, type a maximum value!");
             string userMax = System.Console.ReadLine();
-            int userMaxInt = Convert.ToInt32(userMax);
-            int userNumber = userMaxInt / 2;
-            if (userMaxInt % 2 ==1) userMaxInt++;
-            bool errorOccured=false;
-            int numberOfQuestions = Convert.ToInt32(Math.Log(Convert.ToDouble(userMaxInt),2));
-            System.Console.WriteLine("So, I'm going to ask you {0} questions !", numberOfQuestions);
-            for (int logUserMax = 0; logUserMax < numberOfQuestions-1; logUserMax++)
-            {
-                
-                System.Console.WriteLine("Is your number greater than {0}? (yes/no)", userNumber);
-                string userInput = System.Console.ReadLine();
-                
-                if (userInput=="yes")
-                    {
-                        userMaxInt = userMaxInt / 2;
-                        userNumber = userNumber + userMaxInt/2;
-                    }
-                else if (userInput=="no")
-                    {
-                        userMaxInt = userMaxInt / 2;
-                        userNumber = userNumber - userMaxInt / 2;
-                    }
-                else
-                    {
-                        System.Console.WriteLine("Bad Input! Press any key to exit!");
-                        errorOccured = true;
-                        break;
-                    }
-                
-            }
-            if (!errorOccured)
-            {
-                System.Console.WriteLine("Is your number {0}?(yes/no)", userNumber);
-                string userInputFinal = System.Console.ReadLine();
 
-                if (userInputFinal == "yes")
+            int upperBound = Convert.ToInt32(userMax);
+            int lowerBound = 0;
+            int middlePoint = (upperBound + lowerBound) / 2;
+            int numberOfQuestions = Convert.ToInt32(Math.Ceiling(Math.Log(Convert.ToDouble(upperBound), 2)));
+
+            System.Console.WriteLine("So, I'm going to ask you {0} questions !", numberOfQuestions);
+
+            while (!(lowerBound == upperBound))
+            {
+                System.Console.WriteLine("Is your number greater than {0}? (y/n)",middlePoint);
+
+                string userInput = System.Console.ReadLine();
+                if(userInput=="y")
                 {
-                    System.Console.WriteLine("Haha, you regretted! {0}", userNumber);
+                    lowerBound = middlePoint + 1;
+                    middlePoint = (middlePoint + upperBound)/2;
                 }
-                else if (userInputFinal == "no")
-                    System.Console.WriteLine("So, Your number is {0}", userNumber + 1);
+                else if (userInput=="n")
+                {
+                    upperBound = middlePoint;
+                    middlePoint = (middlePoint + lowerBound) / 2;
+                }
                 else
                 {
                     System.Console.WriteLine("Bad Input! Press any key to exit!");
-                    errorOccured = true;
+                    break;
                 }
+                
             }
-                System.Console.ReadKey();
+            Console.WriteLine("The number is:{0}", upperBound);
+            System.Console.ReadKey();
         }
 
     }
